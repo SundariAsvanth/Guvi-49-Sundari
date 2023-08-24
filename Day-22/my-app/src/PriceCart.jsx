ee07d509d6ac0a2a9b3f7f58e54e3f5a4d6c6ea4
Bala <sundariasvanthkumar@gmail.com>
2023-08-25 00:05:20 +0530

    Task-13 React Day-2

---
+++ Day-22/my-app/src/PriceCart.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+
+export class PriceCart extends Component {
+    
+  render() {
+    return (
+      <div>
+        <div class="wrapper">
+       <div class="table basic">
+           <div class="price-section">
+               <div class="price-area">
+                   <div class="inner-area">
+                       <span class="text">
+                         &#8377;
+                       </span>
+                       <span class="price">499</span>
+                   </div>
+               </div>
+           </div>
+           <div class="package-name">
+    
+           </div>
+           <div class="features">
+               <li>
+                   <span class="list-name">All Content</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Watch on TV or Laptop</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Ads free movies and shows</span>
+                   <span class="icon cross"><i class="far fa-times-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Download videos</span>
+                   <span class="icon cross"><i class="far fa-times-circle"></i></span>
+               </li>
+               <div class="btn"><button>Purchase</button></div>
+           </div>
+       </div>
+       <div class="table Premium">
+           <div class="price-section">
+               <div class="price-area">
+                   <div class="inner-area">
+                       <span class="text">
+                         &#8377;
+                       </span>
+                       <span class="price">899</span>
+                   </div>
+               </div>
+           </div>
+           <div class="package-name">
+            
+           </div>
+           <div class="features">
+               <li>
+                   <span class="list-name">All Content</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Watch on TV or Laptop</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Ads free movies and shows</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Download videos</span>
+                   <span class="icon cross"><i class="far fa-times-circle"></i></span>
+               </li>
+               <div class="btn"><button>Purchase</button></div>
+           </div>
+       </div>
+       <div class="table Ultimate">
+           <div class="price-section">
+               <div class="price-area">
+                   <div class="inner-area">
+                       <span class="text">
+                          &#8377;
+                       </span>
+                       <span class="price">1499</span>
+                   </div>
+               </div>
+           </div>
+           <div class="package-name">
+               
+           </div>
+           <div class="features">
+               <li>
+                   <span class="list-name">All Content</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Watch on TV or Laptop</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Ads free movies and shows</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <li>
+                   <span class="list-name">Download videos</span>
+                   <span class="icon check"><i class="fas fa-check-circle"></i></span>
+               </li>
+               <div class="btn"><button>Purchase</button></div>
+           </div>
+       </div>
+   </div>
+      </div>
+    )
+  }
+}
+
+export default PriceCart

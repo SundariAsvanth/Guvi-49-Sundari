ea9efaa9183df468125a9652e9857c2e4219fcb7
Bala <sundariasvanthkumar@gmail.com>
2024-03-29 19:01:23 +0530

    Tasks Resubmission - Day-1, Day-2, DOM Day-3 Pagination, React Day-3 Shopping Cart.

---
+++ Tasks Resubmission/React Day-3 Shopping cart/shopping-cart/src/App.jsx
@@ -0,0 +1,95 @@
+
+import React, { useState } from 'react';
+import './App.css';
+import ShowCourseComponent from './Show';
+import UserCartComponent from './User';
+ 
+function App() {
+    const [courses, setCourses] = useState([
+        { id: 1, 
+          name: 'T-shirt', 
+          price: 499, 
+          image: 
+'https://media.geeksforgeeks.org/wp-content/uploads/20230823165506/gfg1.png'
+        },
+        { id: 2, 
+          name: 'Bag', 
+          price: 699, 
+          image: 
+          'https://media.geeksforgeeks.org/wp-content/uploads/20230823165553/gfg2.jpg'
+        },
+        { id: 3, 
+          name: 'Hoodie', 
+          price: 799, 
+          image: 
+'https://media.geeksforgeeks.org/wp-content/uploads/20230823165623/gfg3.jpg'
+        }
+    ]);
+ 
+    const [cartCourses, setCartCourses] = useState([]);
+    const [searchCourse, setSearchCourse] = useState('');
+ 
+    const addCourseToCartFunction = (course) => {
+        const alreadyCourses = cartCourses
+                               .find(item => item.product.id === course.id);
+        if (alreadyCourses) {
+            const latestCartUpdate = cartCourses.map(item =>
+                item.product.id === course.id ? { 
+                ...item, quantity: item.quantity + 1 } 
+                : item
+            );
+            setCartCourses(latestCartUpdate);
+        } else {
+            setCartCourses([...cartCourses, {product: course, quantity: 1}]);
+        }
+    };
+ 
+    const deleteCourseFromCartFunction = (course) => {
+        const updatedCart = cartCourses
+                            .filter(item => item.product.id !== course.id);
+        setCartCourses(updatedCart);
+    };
+ 
+    const totalAmountCalculationFunction = () => {
+        return cartCourses
+               .reduce((total, item) => 
+                           total + item.product.price * item.quantity, 0);
+    };
+ 
+    const courseSearchUserFunction = (event) => {
+        setSearchCourse(event.target.value);
+    };
+ 
+    const filterCourseFunction = courses.filter((course) =>
+        course.name.toLowerCase().includes(searchCourse.toLowerCase())
+    );
+ 
+    return (
+      
+        <div className="App">
+           
+                                  <header className="App-header">
+            <h1>ASN Shopping Cart</h1>
+           
+        </header>
+            <main className="App-main">
+                <ShowCourseComponent
+                    courses={courses}
+                    filterCourseFunction={filterCourseFunction}
+                    addCourseToCartFunction={addCourseToCartFunction}
+                />
+ 
+                <UserCartComponent
+                    cartCourses={cartCourses}
+                    deleteCourseFromCartFunction={deleteCourseFromCartFunction}
+                    totalAmountCalculationFunction={
+                        totalAmountCalculationFunction
+                    }
+                    setCartCourses={setCartCourses}
+                />
+            </main>
+        </div>
+    );
+}
+ 
+export default App;
